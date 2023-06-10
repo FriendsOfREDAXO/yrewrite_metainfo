4d78603ffa73fc22363099bd13a96f52e71cafe3
Alexander Walther <aw@alexplus.de>
2023-06-10 17:55:48 +0200

    introduce css and js selector

--- fragments/yrewrite_metainfo/head.php
+++ fragments/yrewrite_metainfo/head.php
@@ -25,32 +25,59 @@ if ($icon = $domain->getIcon()) {
 <!-- YRewrite Meta-Infos Icon-Profil -->
 <?php if($icon->getTouchIcon()) { ?>
 <link rel="apple-touch-icon" sizes="180x180"
-    href="<?= $icon->getTouchIcon() ?>">
+	href="<?= $icon->getTouchIcon() ?>">
 <?php } ?>
 <?php if($icon->getIcon(32)) { ?>
 <link rel="icon" type="image/png" sizes="32x32"
-    href="<?= $icon->getIcon(32) ?>">
+	href="<?= $icon->getIcon(32) ?>">
 <?php } ?>
 <?php if($icon->getIcon(16)) { ?>
 <link rel="icon" type="image/png" sizes="16x16"
-    href="<?= $icon->getIcon(16) ?>">
+	href="<?= $icon->getIcon(16) ?>">
 <?php } ?>
 <?php if($icon->getManifest()) { ?>
 <link rel="manifest" href="<?= $icon->getManifest() ?>">
 <?php } ?>
 <?php if($icon->getPinnedTab()) { ?>
 <link rel="mask-icon" href="<?= $icon->getPinnedTab() ?>"
-    color="<?= $icon->getThemeColor() ?>">
+	color="<?= $icon->getThemeColor() ?>">
 <?php } ?>
 <?php if($icon->getFavicon()) { ?>
 <link rel="shortcut icon" href="<?= $icon->getFavicon() ?>">
 <?php } ?>
 <meta name="apple-mobile-web-app-title"
-    content="<?= $icon->getShortName() ?>">
+	content="<?= $icon->getShortName() ?>">
 <meta name="application-name" content="<?= $icon->getShortName() ?>">
 <meta name="msapplication-TileColor"
-    content="<?= $icon->getTitleColor() ?>">
+	content="<?= $icon->getTitleColor() ?>">
 <meta name="theme-color" content="<?= $icon->getThemeColor() ?>">
 <!-- / YRewrite Meta-Infos Icon-Profil -->
 <?php
 } // $icon
+
+foreach($domain->getStyles() as $style) {
+    ?>
+<link href="/assets/styles/<?= $style ?>" rel="stylesheet">
+
+<?php
+}
+
+foreach($domain->getScripts() as $script) {
+    ?>
+<script src="/assets/scripts/<?= $script ?>"></script>
+
+
+<?php
+}
+
+if (\rex_addon::get('speed_up') && \rex_addon::get('speed_up')->isAvailable()) {
+    $speed_up = new speed_up();
+    $speed_up->show();
+}
+
+if (rex_addon::get('wenns_sein_muss') && rex_addon::get('wenns_sein_muss')->isAvailable()) {
+    
+    echo wsm_fragment::getCss();
+    echo wsm_fragment::getScripts();
+    echo wsm_fragment::getJs();
+} ?>
