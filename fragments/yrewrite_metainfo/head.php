4aea40257019403f5e0c21bb781e8fa79ec17fdf
Norbert Micheel <nm@lautundschoen.de>
2024-07-23 15:30:04 +0200

    Update fragments/yrewrite_metainfo/head.php
    Co-authored-by: Gregor Harlan <mail@gh01.de>

--- fragments/yrewrite_metainfo/head.php
+++ fragments/yrewrite_metainfo/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php
-if (rex_addon::get('url') && rex_addon::get('url')->isAvailable()) {
+if (rex_addon::get('url')->isAvailable()) {
     $seo = new \Url\Seo();
 } else {
     $seo = new rex_yrewrite_seo();
