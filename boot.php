3266aa12cb65a9d26162e933bd3eafdcba1f0b24
Alexander Walther <mail@alexplus.de>
2024-08-09 13:37:39 +0200

    Buttons zum Hinzufügen und Editieren in YRewrite-Domain per EP einfügen.

--- boot.php
+++ boot.php
@@ -2,6 +2,7 @@
 
 namespace Alexplusde\YrewriteMetainfo;
 
+use rex_i18n;
 use rex_yform_manager_dataset;
 
 rex_yform_manager_dataset::setModelClass(
@@ -13,5 +14,34 @@ rex_yform_manager_dataset::setModelClass(
     Icon::class,
 );
 
-// rex_extension::register('REX_YFORM_SAVED', function (rex_extension_point $ep) {
-// });
+// Listendarstellung von YRewrite Domains um eine Spalte ergänzen mit Link zu YRewrite Metainfos
+\rex_extension::register('REX_LIST_GET', function (\rex_extension_point $ep) {
+    $list = $ep->getSubject();
+    /** @var \rex_list $list */
+    $list->addColumn(rex_i18n::msg('yrewrite_metainfo_title'), '', 3);
+    $list->setColumnFormat(rex_i18n::msg('yrewrite_metainfo_title'), 'custom', function ($a) {
+
+        $_csrf_key = \rex_yform_manager_table::get('rex_yrewrite_metainfo')->getCSRFKey();
+        $token = \rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+        $params = [];
+        $params['table_name'] = 'rex_yrewrite_metainfo';
+        $params['rex_yform_manager_popup'] = '0';
+        $params['_csrf_token'] = $token['_csrf_token'];
+        $params['data_id'] = $a['list']->getValue('id');
+        $params['func'] = 'add';
+
+        $domain = Domain::get($a['list']->getValue('id'));
+        if ($domain) {
+            $params['func'] = 'edit';
+            return '<a href="' . \rex_url::backendPage('yrewrite/metainfo/domain', $params) . '">' . rex_i18n::msg('yrewrite_metainfo_edit') . '</a>';
+        }
+        // Link zu Neue YRewrite-Metainfo-Domain erstellen
+        // https://blaupause.test/redaxo/index.php?func=add&page=yrewrite%2Fmetainfo%2Fdomain&table_name=rex_yrewrite_metainfo&list=&sort=&sorttype=&start=0&_csrf_token=s_AUawHLnRx4uZib-ACHIs9nnbd0tpYpHQP_Vb4PxAg&rex_yform_manager_popup=0
+
+
+        return '<a href="' . \rex_url::backendPage('yrewrite/metainfo/domain', $params) . '">' . rex_i18n::msg('yrewrite_metainfo_add') . '</a>';
+
+    });
+    return $list;
+});
