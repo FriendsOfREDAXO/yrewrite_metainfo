e253ca0fd63d5f663c8da16b27a66c308c3d1e4d
Alexander Walther <mail@alexplus.de>
2024-08-19 13:15:45 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,3 @@
 <?php
 
-/* Tablesets aktualisieren */
-$addon = rex_addon::get('yrewrite_metainfo');
-
-if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
-    rex_yform_manager_table_api::importTablesets((string) rex_file::get(__DIR__ . '/install/rex_yrewrite_metainfo.tableset.json'));
-    rex_yform_manager_table::deleteCache();
-}
+include __DIR__ . 'install.php';
